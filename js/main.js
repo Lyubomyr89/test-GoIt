8575dffb5bb38c892145e60932eb5aa09e3a268e
lyubchuk89 <lyubchukgorbatyuk@gmail.com>
2020-03-25 12:55:12 +0200

    ready-project

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var menuItem = Array.from(document.getElementsByClassName('menu__item'));
+
+menuItem.forEach(function (item) {
+    item.onclick = function () {
+        menuItem.forEach(function (thiss) {
+            thiss.classList.remove('active');
+        });
+        item.classList.add('active');
+    }
+});
